7e986f069b857ef9564108ee8742e24998c9654d
Cristobal <cristobaljohn00@gmail.com>
2020-03-24 12:25:40 -0600

    pro 1

---
+++ 01_Textos/01_Textos/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  01_Textos
+//
+//  Created by John Alexis Cristobal Jimenez  on 3/24/20.
+//  Copyright © 2020 johnvera. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
