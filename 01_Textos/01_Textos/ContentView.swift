1b4a50fbded68110e1e9a56561669d69a3a62454
John_Vera <johnalexiscristobaljimenez6@gmail.com>
2020-03-24 23:25:24 -0600

    multilinea

--- 01_Textos/01_Textos/ContentView.swift
+++ 01_Textos/01_Textos/ContentView.swift
@@ -10,13 +10,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum")
         .fontWeight(.regular) //fuente negrita
         //.font(.largeTitle)   //tamaño fuente
         //.font(.system(.largeTitle, design: .rounded))   //estilo redondeado
         //.font(.system(size: 35, design: .rounded))   //estilo redondeado y con tamaño fijo
-            .font(.custom("Gill Sans", size: 25))   //estilo helvetica y 25
+        .font(.custom("Gill Sans", size: 25))   //estilo helvetica y 25
         .foregroundColor(.blue) //color de texto o con UIColor
+        .multilineTextAlignment(.center)    //alinear text
+        .lineLimit(3)   //solo 3 lineas
+        .truncationMode(.head)
+        
+        
     }
 }
 
