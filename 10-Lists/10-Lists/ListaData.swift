690c9f6e6533855f2ef6954ee0079776c75ed780
Cristobal <cristobaljohn00@gmail.com>
2020-04-27 11:26:56 -0500

    curso listas full

--- 10-Lists/10-Lists/ListaData.swift
+++ 10-Lists/10-Lists/ListaData.swift
@@ -23,28 +23,60 @@ struct ListaData: View {
         //en este caso usamos el nombre temporalment
         
         //pero si usamos protocolo identificable, ahorramos una linea 
-        List(cursos) { curso in
-            HStack{
-                Image(curso.image)
-                //con resizable se ajusta la imagen al tamaño del frame
-                .resizable()
-                //modo de presentar imagen
-                .aspectRatio(contentMode: .fill)
-                //frame para tamaño
-                .frame(width: 60, height: 60)
-                .clipped()
-                //redondeamos para circulo
-                .cornerRadius(30)
-            
-                Text("\(curso.name)")
+        List(cursos.indices) { idx in
+            if idx < 2{
+                SubFullView(curso: self.cursos[idx])
+            }else{
+                SubView(curso: self.cursos[idx])
             }
         }
     }
 }
 
-struct ListaData_Previews: PreviewProvider {
-    static var previews: some View {
-        ListaData()
+struct SubView: View {
+    var curso: Curso
+    var body: some View {
+        HStack{
+            Image(curso.image)
+            //con resizable se ajusta la imagen al tamaño del frame
+            .resizable()
+            //modo de presentar imagen
+            .aspectRatio(contentMode: .fill)
+            //frame para tamaño
+            .frame(width: 60, height: 60)
+            .clipped()
+            //redondeamos para circulo
+            .cornerRadius(30)
+        
+            Text("\(curso.name)")
+        }
+    }
+}
+
+//una fila con asepcto diferente
+struct SubFullView: View {
+    
+    var curso: Curso
+    var body: some View {
+        HStack{
+            Image(curso.image)
+                .resizable()    //para redimensionar
+                .aspectRatio(contentMode: .fill)    //para no deformar imagen
+                .frame(height: 200) //dejarlo con un alto de 200
+                .cornerRadius(15)
+                //agrega capa gris para restrle importancia
+                .overlay(
+                    RoundedRectangle(cornerRadius: 15)
+                        .foregroundColor(.black)
+                        .opacity(0.35)
+                )
+            
+            Text("\(curso.name)")
+                .font(.system(.headline, design: .rounded))
+                .bold()
+                .foregroundColor(.white)
+                .multilineTextAlignment(.center)
+        }
     }
 }
 
@@ -57,4 +89,10 @@ struct Curso : Identifiable {
     //numero de 128 bits
     var name: String
     var image: String
+}
+
+struct ListaData_Previews: PreviewProvider {
+    static var previews: some View {
+        ListaData()
+    }
 }
